93b45e02be2b00be83895829498cf8025f2f76bb
nabila-maker <nabila@MacBook-Pro-de-Nabila.local>
2021-01-29 22:51:51 +0100

    update footer

--- main.js
+++ main.js
@@ -15,12 +15,3 @@ if (window.pageYOffset > 0) {
 }
 });
 
-//let counter = 1;
-
-//setInterval(function(){
-  //  document.getElementById("radio" + counter).checked = true;
-  //  counter++;
- //   if (counter > 4) {
- //       counter = 1;
-//    }
-//}, 5000);
