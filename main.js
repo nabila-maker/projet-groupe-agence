2b8b1ccd45898cc9392f4bacd5cf3ee7ade36326
nabila-maker <nabila@MacBook-Pro-de-Nabila.local>
2021-01-29 14:27:37 +0100

    update css html

---
+++ main.js
@@ -0,0 +1,26 @@
+
+window.addEventListener("scroll", function(){
+    const logo = document.querySelector(".logo img");
+    const mainNav = document.querySelector("#mainNav");
+
+
+if (window.pageYOffset > 0) {
+    logo.style.height = "75px";
+    mainNav.classList.add("bg-black");
+    mainNav.classList.add("txt-white");
+} else {
+    logo.style.height = "85px";
+    mainNav.classList.remove("bg-black");
+    mainNav.classList.remove("txt-white");
+}
+});
+
+//let counter = 1;
+
+//setInterval(function(){
+  //  document.getElementById("radio" + counter).checked = true;
+  //  counter++;
+ //   if (counter > 4) {
+ //       counter = 1;
+//    }
+//}, 5000);
